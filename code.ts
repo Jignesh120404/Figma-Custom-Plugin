41115eee5d0d751a210679045eaa6c693461ced3
Jignesh Patil <120722412+Jignesh120404@users.noreply.github.com>
2023-11-19 13:28:06 +0530

    Update code.ts

--- code.ts
+++ code.ts
@@ -6,15 +6,15 @@ figma.ui.onmessage = (msg) => {
   if (msg.type === "create-rectangles") {
     const { values } = msg;
 
-    // Iterate over each item in the values array
+    
     values.forEach((item) => {
-      // Create a Figma node from the SVG content
+      
       const icon = figma.createNodeFromSvg(item.content);
 
-      // Set the name of the created node
+      
       icon.name = item.name;
 
-      // Customize the appearance of the node (background, position, size, etc.)
+    
       icon.backgrounds = [
         { type: "SOLID", opacity: 0, color: { r: 0, g: 0, b: 0 } },
       ];
@@ -22,10 +22,9 @@ figma.ui.onmessage = (msg) => {
       icon.y = figma.viewport.center.y;
       icon.resize(100, 100);
 
-      // Append the node to the current Figma page
       figma.currentPage.appendChild(icon);
 
-      // Select the created node and scroll/zoom it into view
+      
       figma.currentPage.selection = [icon];
       figma.viewport.scrollAndZoomIntoView([icon]);
     });
